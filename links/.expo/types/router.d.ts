9c48773293f1154e3353ac351827890d2cdfba7a
mnscmart <aiorosdesagitario13@gmail.com>
2024-12-30 16:05:34 -0400

    project start

---
+++ links/.expo/types/router.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import * as Router from 'expo-router';
+
+export * from 'expo-router';
+
+declare module 'expo-router' {
+  export namespace ExpoRouter {
+    export interface __routes<T extends string | object = string> {
+      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/../componets/category/index`; params?: Router.UnknownInputParams; } | { pathname: `/../componets/category/styles`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/index`; params?: Router.UnknownInputParams; } | { pathname: `/index/styles`; params?: Router.UnknownInputParams; };
+      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/../componets/category/index`; params?: Router.UnknownOutputParams; } | { pathname: `/../componets/category/styles`; params?: Router.UnknownOutputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `/index`; params?: Router.UnknownOutputParams; } | { pathname: `/index/styles`; params?: Router.UnknownOutputParams; };
+      href: Router.RelativePathString | Router.ExternalPathString | `/../componets/category/index${`?${string}` | `#${string}` | ''}` | `/../componets/category/styles${`?${string}` | `#${string}` | ''}` | `/_sitemap${`?${string}` | `#${string}` | ''}` | `/index${`?${string}` | `#${string}` | ''}` | `/index/styles${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/../componets/category/index`; params?: Router.UnknownInputParams; } | { pathname: `/../componets/category/styles`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/index`; params?: Router.UnknownInputParams; } | { pathname: `/index/styles`; params?: Router.UnknownInputParams; };
+    }
+  }
+}
---
+++ links/src/app/index/index.tsx
@@ -0,0 +1,26 @@
+import { Image , View,TouchableOpacity }from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+
+import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+
+import { Category } from "@/componets/category"
+
+export default function Index() {
+    return (
+    <View style={styles.container}> 
+    <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+
+        <TouchableOpacity>
+        <MaterialIcons name="add" size={32} color={colors.green[300]}/>
+        </TouchableOpacity>
+
+    </View>
+    <Category />
+    </View>
+    )
+}
+
