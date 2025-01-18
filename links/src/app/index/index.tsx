898cd53eee9a4a3b4fc9cf01f6d50f7afb70baff
mnscmart <aiorosdesagitario13@gmail.com>
2025-01-18 10:32:11 -0400

    criando uma tela e acão para o botão Add

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,5 +1,6 @@
 import { Image , View,TouchableOpacity,FlatList,Modal,Text}from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { Route, router } from "expo-router"
 
 
 
@@ -17,7 +18,7 @@ export default function Index() {
         <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/add")}>
         <MaterialIcons name="add" size={32} color={colors.green[300]}/>
         </TouchableOpacity>
 
@@ -40,7 +41,7 @@ export default function Index() {
     contentContainerStyle={styles.linksContent}
     showsVerticalScrollIndicator={false}
     />
-    <Modal transparent visible={true}>
+    <Modal transparent visible={false}>
         <View style={styles.modal}>
             <View style={styles.modalContent}>
                 <View style={styles.modalHeader}>
